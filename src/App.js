9b96f960a0efe7528eca6865d17e3dd87f4533ce
HongseokChoi <xjx06127@inha.edu>
2023-09-21 22:12:56 +0900

    1페이지 완성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { GlobalStyle } from "./GlobalStyle";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Blue from "./pages_first/Blue";
+import Green from "./pages_first/Green";
+import Pink from "./pages_first/Pink";
+import Purple from "./pages_first/Purple";
+import Red from "./pages_first/Red";
+import Yellow from "./pages_first/Yellow";
+
 function App() {
   return (
-    <div>
-      <h1>first setting!</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/green" element={<Green />} />
+          <Route path="/pink" element={<Pink />} />
+          <Route path="/purple" element={<Purple />} />
+          <Route path="/red" element={<Red />} />
+          <Route path="/yellow" element={<Yellow />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages_first/Yellow.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+
+const LetsImg = styled.img`
+  content: url("Lets_g.svg");
+  width: 100%;
+`;
+
+const ColorImg = styled.img`
+  content: url("Color_y.svg");
+  width: 165%;
+  object-fit: cover;
+`;
+
+const ColorCon = styled.div`
+  width: 100%;
+  overflow: hidden;
+  margin-top: 5%;
+  display: flex;
+  justify-content: center;
+`;
+
+const ExplainImg = styled.image`
+  content: url("explain_y.svg");
+  width: 100%;
+  margin-left: 8%;
+  margin-top: 5%;
+`;
+
+const BottomCon = styled.div`
+  width: 100%;
+  overflow: hidden;
+  margin-top: 5%;
+  display: flex;
+  justify-content: center;
+`;
+
+const Con = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Yellow = () => {
+  return (
+    <Con>
+      <LetsImg />
+      <ColorCon>
+        <ColorImg />
+      </ColorCon>
+      <BottomCon>
+        <ExplainImg />
+      </BottomCon>
+    </Con>
+  );
+};
+
+export default Yellow;
